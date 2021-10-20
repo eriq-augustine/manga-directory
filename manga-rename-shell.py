a6b475da48493ee31452701f5c0cc529db7fde0b
Eriq Augustine <eriq.augustine@gmail.com>
2021-10-20 15:53:10 -0700

    Changed how reload and bulk work. Now they will both try to use a defined regex to pull out a number and only rely on the positional number of that search fails.

--- manga-rename-shell.py
+++ manga-rename-shell.py
@@ -10,7 +10,8 @@ import sys
 DIRECTORY_REGEX   = re.compile(r'^(.+) v(\d{3}) c(\d{3}[a-z]?)$')
 ARCHIVE_REGEX     = re.compile(r'^(.+) v(\d{3}) c(\d{3}[a-z]?)\.(cbz)$')
 PAGE_REGEX        = re.compile(r'^(.+) v(\d{3}) c(\d{3}[a-z]?) p(\d{3}(?:-\d{3})?[a-z]?)\.(jpg|png|webp)$')
-PAGE_NUMBER_REGEX = re.compile(r'(\d+)(?:-(\d+))?([a-z])?')
+
+NUMBER_REGEX = re.compile(r'((\d+)(?:-(\d+))?([a-z])?)')
 
 CHECKMARK = '✓'
 
@@ -44,19 +45,7 @@ class RenameShell(cmd.Cmd):
             print('ERROR: Found no pattern for bulk rename.')
             return
 
-        for i in range(len(self.renames)):
-            original, newName = self.renames[i]
-            ext = os.path.splitext(original)[-1]
-
-            match = re.search(arg, original)
-            if (match is None or len(match.groups()) != 1):
-                continue
-
-            paddedNumber = self._parseAndPad(match.group(1))
-            if (paddedNumber is None):
-                continue
-
-            self.renames[i][1] = "%s p%s%s" % (self.baseName, paddedNumber, ext)
+        self._reload(numberRegex = arg)
 
     def do_cd(self, arg):
         arg = arg.strip()
@@ -109,10 +98,10 @@ class RenameShell(cmd.Cmd):
     def do_help(self, arg):
         print('? / help - Display this prompt.')
         print('bulk     - Take a given regex and register new renames for all entries.')
-        print('             The pattern must capture the page number in the first capture group.')
-        print('             re.search is used.')
-        print('             On a failed match, no change will be made to the entry.')
-        print('             Even entries marked for ignore or deletion will get their rename changed, but their action will remain.')
+        print('           The pattern must capture the context number in the first capture group.')
+        print('           re.search is used.')
+        print('           On a failed match, no change will be made to the entry.')
+        print('           Even entries marked for ignore or deletion will get their rename changed, but their action will remain.')
         print('cd       - Chance the current working directory.')
         print('rm       - Delete a single entry from disk.')
         print('edit     - Edit a single rename entry.')
@@ -121,7 +110,7 @@ class RenameShell(cmd.Cmd):
         print('quit     - Quit out of interactive mode without writing/saving the results.')
         print('reload   - Reload this directory from disk.')
         print('type     - Set the type for this directory:')
-        print('             (n)one, (s)eries, or (c)hapter.')
+        print('           (n)one, (s)eries, or (c)hapter.')
         print('write    - Write all the renames to disk and quit interactive mode.')
 
     def do_ls(self, arg):
@@ -178,7 +167,6 @@ class RenameShell(cmd.Cmd):
     def do_write(self, arg):
         self._commit()
         print('Renames committed to disk.')
-        return True
 
     def do_EOF(self, arg):
         return self.do_quit(None)
@@ -200,24 +188,25 @@ class RenameShell(cmd.Cmd):
             elif (action == ACTION_DELETE):
                 self._remove(originalPath)
 
-    def _remove(self, path):
-        if (os.path.isfile(path) or os.path.islink(path)):
-            os.remove(path)
-        elif os.path.isdir(path):
-            shutil.rmtree(path)
-        else:
-            raise ValueError("Path %s is not a file, link, or dir." % (path))
-
-    def _createRename(self, original, number):
+    # The regex should pull out the number in the first capture group.
+    def _createRename(self, original, backupNumber, numberRegex):
         ext = os.path.splitext(original)[-1]
-        number = self._parseAndPad(number)
+
+        number, highestNumber = self._parseAndPad(backupNumber)
+
+        matches = re.findall(numberRegex, original)
+        if (matches is not None and len(matches) > 0):
+            number, highestNumber = self._parseAndPad(matches[-1][0])
+
+        if (number is None):
+            return original, None
 
         if (self.dirType == TYPE_NONE):
-            return original
+            return original, highestNumber
         elif (self.dirType == TYPE_SERIES):
-            return "%s c%s%s" % (self.baseName, number, ext)
+            return "%s c%s%s" % (self.baseName, number, ext), highestNumber
         elif (self.dirType == TYPE_CHAPTER):
-            return "%s p%s%s" % (self.baseName, number, ext)
+            return "%s p%s%s" % (self.baseName, number, ext), highestNumber
         else:
             raise ValueError("Unknown directory type: %s." % (self.dirType))
 
@@ -226,19 +215,25 @@ class RenameShell(cmd.Cmd):
     def _parseAndPad(self, text):
         text = str(text).strip()
 
-        match = PAGE_NUMBER_REGEX.match(text)
+        match = NUMBER_REGEX.match(text)
         if (match is None):
-            return None
+            return None, None
 
-        text = "%03d" % (int(match.group(1)))
-
-        if (match.group(2) is not None):
-            text += "-%03d" % (int(match.group(2)))
+        number = int(match.group(2))
+        text = "%03d" % (number)
+        highestNumber = number
 
         if (match.group(3) is not None):
-            text += match.group(3)
+            number = int(match.group(3))
+            text += "-%03d" % (number)
 
-        return text
+            if (number > highestNumber):
+                highestNumber = number
+
+        if (match.group(4) is not None):
+            text += match.group(4)
+
+        return text, highestNumber
 
     def _parseIndex(self, arg):
         arg = str(arg).strip()
@@ -261,13 +256,25 @@ class RenameShell(cmd.Cmd):
 
         return (index, arg)
 
-    def _reload(self):
+    def _reload(self, numberRegex = NUMBER_REGEX):
         self.renames = []
+        nextNumber = 1
 
         for dirent in sorted(os.listdir(self.basePath)):
-            self.renames.append([dirent, self._createRename(dirent, len(self.renames) + 1)])
+            rename, highestNumber = self._createRename(dirent, nextNumber, numberRegex)
+            nextNumber = max(nextNumber, highestNumber) + 1
+
+            self.renames.append([dirent, rename])
 
         self.actions = [ACTION_RENAME] * len(self.renames)
+
+    def _remove(self, path):
+        if (os.path.isfile(path) or os.path.islink(path)):
+            os.remove(path)
+        elif os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            raise ValueError("Path %s is not a file, link, or dir." % (path))
 
 def main(args):
     RenameShell(args.path[0]).cmdloop()
