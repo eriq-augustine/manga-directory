898998843cd5bcdebd7d89a3dae037f7b9cdb994
Eriq Augustine <eriq.augustine@gmail.com>
2021-10-20 09:51:32 -0700

    Changed the script into a full shell.

--- manga-rename-shell.py
+++ manga-rename-shell.py
@@ -19,17 +19,25 @@ ACTION_RENAME = 'R'
 ACTION_IGNORE = 'I'
 ACTION_DELETE = 'D'
 
-class InteractiveMode(cmd.Cmd):
-    def __init__(self, basePath, renames):
+TYPE_NONE = 'None'
+TYPE_SERIES = 'Series'
+TYPE_CHAPTER = 'Chapter'
+
+class RenameShell(cmd.Cmd):
+    def __init__(self, basePath):
         super().__init__()
 
-        self.intro = "Editing directory '%s'. Type 'help' or '?' for help." % (basePath)
-        self.prompt = '> '
-
-        self.basePath = basePath
+        self.basePath = os.path.abspath(basePath)
         self.baseName = os.path.basename(self.basePath)
-        self.renames = renames
-        self.actions = [ACTION_RENAME] * len(self.renames)
+        self.dirType = TYPE_NONE
+
+        self.renames = []
+        self.actions = []
+
+        self.intro = "Editing directory '%s'. Type 'help' or '?' for help." % (self.basePath)
+        self.prompt = "%s > " % (self.basePath)
+
+        self._reload()
 
     def do_bulk(self, arg):
         arg = arg.strip()
@@ -51,34 +59,30 @@ class InteractiveMode(cmd.Cmd):
 
             self.renames[i][1] = "%s p%s%s" % (self.baseName, paddedNumber, ext)
 
-    # Take in some text that is supposed to be a page number and return a padded version.
-    # Note that "page numbers" are not just digits, they can have a dash and letters.
-    def _parseAndPad(self, text):
-        text = text.strip()
+    def do_cd(self, arg):
+        arg = arg.strip()
 
-        match = PAGE_NUMBER_REGEX.match(text)
-        if (match is None):
-            return None
+        path = arg
+        if (not os.path.isabs(path)):
+            path = os.path.join(self.basePath, path)
 
-        text = "%03d" % (int(match.group(1)))
+        self.basePath = os.path.abspath(path)
+        self.baseName = os.path.basename(self.basePath)
 
-        if (match.group(2) is not None):
-            text += "-%03d" % (int(match.group(2)))
+        self._reload()
 
-        if (match.group(3) is not None):
-            text += match.group(3)
-
-        return text
-
-    def do_delete(self, arg):
+    def do_edit(self, arg):
         index, arg = self._parseIndex(arg)
         if (index is None):
             return
 
-        path = os.path.join(self.basePath, self.renames[index][0])
+        if (arg is None):
+            print('ERROR: No new name specified.')
+            return
 
-        print("Index %d (%s) marked for delete." % (index, path))
-        self.actions[index] = ACTION_DELETE
+        print("Editing rename index %d: '%s' -> '%s'." % (index, self.renames[index][1], arg))
+        self.actions[index] = ACTION_RENAME
+        self.renames[index][1] = arg
 
     def do_ignore(self, arg):
         index, arg = self._parseIndex(arg)
@@ -89,20 +93,26 @@ class InteractiveMode(cmd.Cmd):
         self.actions[index] = ACTION_IGNORE
 
     def do_help(self, arg):
-        print('? / (h)elp - Display this prompt.')
-        print('(d)elete   - Delete a single entry from disk.')
-        print('(i)gnore   - Ignore a single entry (remove it from the list of renames).')
-        print('(p)rint    - Print all the current renames.')
-        print('(q)uit     - Quit out of interactive mode without writing/saving the results.')
-        print('(r)ename   - Rename a single entry.')
-        print('(w)rite    - Write all the renames to disk and quit interactive mode.')
-        print('(b)ulk     - Take a given regex and register new renames for all entries.')
+        print('? / help - Display this prompt.')
+        print('bulk     - Take a given regex and register new renames for all entries.')
         print('             The pattern must capture the page number in the first capture group.')
         print('             re.search is used.')
         print('             On a failed match, no change will be made to the entry.')
         print('             Even entries marked for ignore or deletion will get their rename changed, but their action will remain.')
+        print('cd       - Chance the current working directory.')
+        print('rm       - Delete a single entry from disk.')
+        print('edit     - Edit a single rename entry.')
+        print('ignore   - Ignore a single entry (remove it from the list of renames).')
+        print('ls       - List the entries in the current directory.')
+        print('quit     - Quit out of interactive mode without writing/saving the results.')
+        print('reload   - Reload this directory from disk.')
+        print('type     - Set the type for this directory:')
+        print('             (n)one, (s)eries, or (c)hapter.')
+        print('write    - Write all the renames to disk and quit interactive mode.')
 
-    def do_print(self, arg):
+    def do_ls(self, arg):
+        print("%s (%s)" % (self.basePath, self.dirType))
+
         for i in range(len(self.renames)):
             action = self.actions[i]
             original, newName = self.renames[i]
@@ -120,18 +130,36 @@ class InteractiveMode(cmd.Cmd):
         print('Quitting without writting renames.')
         return True
 
-    def do_rename(self, arg):
+    def do_reload(self, arg):
+        self._reload()
+        print('Directory reloaded.')
+
+    def do_rm(self, arg):
         index, arg = self._parseIndex(arg)
         if (index is None):
             return
 
-        if (arg is None):
-            print('ERROR: No new name specified.')
+        path = os.path.join(self.basePath, self.renames[index][0])
+
+        print("Index %d (%s) marked for delete." % (index, path))
+        self.actions[index] = ACTION_DELETE
+
+    def do_type(self, arg):
+        newType = None
+
+        arg = arg.strip().lower()
+        if (arg == '' or arg[0] == TYPE_NONE[0].lower()):
+            newType = TYPE_NONE
+        elif (arg[0] == TYPE_SERIES[0].lower()):
+            newType = TYPE_SERIES
+        elif (arg[0] == TYPE_CHAPTER[0].lower()):
+            newType = TYPE_CHAPTER
+        else:
+            print("ERROR: Unknown type '%s'." % (arg))
             return
 
-        print("Renaming index %d: '%s' -> '%s'." % (index, self.renames[index][1], arg))
-        self.actions[index] = ACTION_RENAME
-        self.renames[index][1] = arg
+        print("Setting directory type to %s." % (newType))
+        self.dirType = newType
 
     def do_write(self, arg):
         self._commit()
@@ -166,7 +194,41 @@ class InteractiveMode(cmd.Cmd):
         else:
             raise ValueError("Path %s is not a file, link, or dir." % (path))
 
+    def _createRename(self, original, number):
+        ext = os.path.splitext(original)[-1]
+        number = self._parseAndPad(number)
+
+        if (self.dirType == TYPE_NONE):
+            return original
+        elif (self.dirType == TYPE_SERIES):
+            return "%s c%s%s" % (self.baseName, number, ext)
+        elif (self.dirType == TYPE_CHAPTER):
+            return "%s p%s%s" % (self.baseName, number, ext)
+        else:
+            raise ValueError("Unknown directory type: %s." % (self.dirType))
+
+    # Take in some text that is supposed to be a page number and return a padded version.
+    # Note that "page numbers" are not just digits, they can have a dash and letters.
+    def _parseAndPad(self, text):
+        text = str(text).strip()
+
+        match = PAGE_NUMBER_REGEX.match(text)
+        if (match is None):
+            return None
+
+        text = "%03d" % (int(match.group(1)))
+
+        if (match.group(2) is not None):
+            text += "-%03d" % (int(match.group(2)))
+
+        if (match.group(3) is not None):
+            text += match.group(3)
+
+        return text
+
     def _parseIndex(self, arg):
+        arg = str(arg).strip()
+
         match = re.match(r'^(-?\d+)\s*.*$', arg)
         if (match is None):
             print('ERROR: Expecting index.')
@@ -185,77 +247,23 @@ class InteractiveMode(cmd.Cmd):
 
         return (index, arg)
 
-# Make useful aliases (short and caps) for the commands.
-for entry in dir(InteractiveMode):
-    if (not entry.startswith('do_') or entry == 'do_EOF'):
-        continue
+    def _reload(self):
+        self.renames = []
 
-    basename = entry.removeprefix('do_')
+        for dirent in sorted(os.listdir(self.basePath)):
+            self.renames.append([dirent, self._createRename(dirent, len(self.renames) + 1)])
 
-    setattr(InteractiveMode, 'do_' + basename.upper(), getattr(InteractiveMode, entry))
-    setattr(InteractiveMode, 'do_' + basename[0], getattr(InteractiveMode, entry))
-    setattr(InteractiveMode, 'do_' + basename[0].upper(), getattr(InteractiveMode, entry))
-
-def interactiveMode(basePath, renames):
-    InteractiveMode(basePath, renames).cmdloop()
-
-def editDir(basePath):
-    renames = []
-
-    baseName = os.path.basename(basePath)
-
-    for dirent in sorted(os.listdir(basePath)):
-        path = os.path.join(basePath, dirent)
-
-        if (os.path.isfile(path)):
-            # If this is a non-archive file, assume it is a page.
-
-            ext = os.path.splitext(dirent)[-1]
-
-            match = PAGE_REGEX.match(dirent)
-
-            if (ext[1:] == 'cbz'):
-                renames.append([dirent, dirent])
-            elif (match is not None):
-                renames.append([dirent, dirent])
-            else:
-                renames.append([dirent, "%s p%03d%s" % (baseName, len(renames) + 1, ext)])
-        else:
-            # If this is a directory, assume it is a chapter.
-
-            match = DIRECTORY_REGEX.match(dirent)
-            if (match is not None):
-                renames.append([dirent, dirent])
-            else:
-                renames.append([dirent, "%s c%03d" % (baseName, len(renames) + 1)])
-
-    interactiveMode(basePath, renames)
-
-def editDirs(paths):
-    for path in paths:
-        editDir(path)
-
-def validateDirs(paths):
-    # TODO
-    print("TODO")
-    return 1
+        self.actions = [ACTION_RENAME] * len(self.renames)
 
 def main(args):
-    if (args.interactive):
-        return editDirs(args.paths)
-    else:
-        return validateDirs(args.path)
+    RenameShell(args.path[0]).cmdloop()
 
 def _load_args(args):
-    parser = argparse.ArgumentParser(description = 'Validate manga directories or interactively edit them.')
+    parser = argparse.ArgumentParser(description = 'Interactively edit managa directories.')
 
-    parser.add_argument('-i', '--interactive', dest = 'interactive',
-            action = 'store_true', default = False,
-            help = 'interactively edit dirents')
-
-    parser.add_argument('paths',
-            metavar = 'PATH', type = str, nargs = '+',
-            help = 'a manga directory')
+    parser.add_argument('path',
+            metavar = 'PATH', type = str, nargs = 1,
+            help = 'a directory')
 
     return parser.parse_args()
 
